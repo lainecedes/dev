9c4db7efbc04769fd306336610f296221d546a04
lainecedes <elaine.parieh@gmail.com>
2025-07-10 20:11:18 +0200

    init project

---
+++ i18n/i18n.config.ts
@@ -0,0 +1,12 @@
+import en from './en.json'
+import nl from './nl.json'
+
+export default {
+    legacy: false,
+    locale: 'nl',
+    fallbackLocale: 'nl',
+    messages: {
+        en,
+        nl,
+    },
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+    css: ['@/assets/css/tailwind.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+
+    modules: [
+        '@nuxtjs/i18n',
+        '@nuxt/content',
+        '@nuxt/image',
+        '@nuxtjs/tailwindcss'
+    ],
+    i18n: {
+        locales: ['en', 'nl'],
+        defaultLocale: 'nl',
+        strategy: 'prefix',
+        vueI18n: './i18n/i18n.config.ts',
+        experimental: {
+            bundle: {
+                optimizeTranslationDirective: false,
+            },
+        },
+    },
+})
